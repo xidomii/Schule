3ff792777f76251bcb00ec5543f35121f25f431f
xidomii <dheiss0407@gmail.com>
2025-05-13 16:02:00 +0200

    fertiger sql_injection code

--- sql_injection/sql_injection.py
+++ sql_injection/sql_injection.py
@@ -5,16 +5,23 @@ try:
         host = "localhost",
         username = "domi",
         password = "0407",
-        database ="mitarbeiter"
+        database = "mitarbeiter"
     )
     cursor = connection.cursor()
 
+    cursor.execute("use mitarbeiter;")
+
     username = input("Benutzername: ")
     password = input("Passwort: ")
-
-    sql_query = f"SELECT * FROM mitarbeiter WHERE username = '{username}' AND password = '{password}'"
-
+    """
+    ACHTUNG: Vulnerable to SQL Injection!
+    sql_query = f"SELECT * FROM mitarbeiter WHERE username = '{username}' AND password = '{password}';"
     cursor.execute(sql_query)
+    """
+
+    # Lösung: Parametrisierter Input (cursor.execute)
+    parameters = (username, password)
+    cursor.execute("SELECT * FROM mitarbeiter WHERE username = %s AND password = %s", parameters)
 
     record = cursor.fetchall()
 
@@ -22,7 +29,7 @@ try:
         for r in record:
             print(r)
     else:
-        print("Fehler")
+        print("Fehler, Kein Zugriff!")
 
 except Error as e:
     print(f"Fehler: {e}")
